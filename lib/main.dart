f119aa91a3545d71e211a7760fb3fa17301876a4
Nishant-R10 <nishanth.r@omnicoredigital.com>
2025-11-19 16:22:07 +0530

    Update transaction popup design/logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'features/beneficiaries/presentation/pages/beneficiaries_screen.dart';
+import 'package:beneficiary/features/transaction_popup/presentation/widgets/transaction_detail_popup.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(       
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,9 +11,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final tx = TransactionDetail(
+      amount: "+23,489.28 QAR",
+      date: "23/11/2025, 23:07 PM",
+      referenceNumber: "ABCDE12345",
+      beneficiaryName: "Sara Rahman",
+      bankAndAccount: "Doha Bank\nAccount No: 1234567890",
+      currency: "QAR",
+      status: "Success",
+      channel: "Mobile Banking",
+      swiftReference: "SW12345678",
+      description: "RIB Test Line One\nRIB Test Line Two\nRIB Test Line Three",
+    );
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BeneficiariesScreen(),  
+      home: Builder(
+        builder: (context) {
+          // Open popup immediately
+          Future.microtask(() {
+            showDialog(
+              context: context,
+              builder: (_) => TransactionDetailPopup(transaction: tx),
+            );
+          });
+
+          return const Scaffold(
+            backgroundColor: Colors.white,
+          );
+        },
+      ),
     );
   }
 }
